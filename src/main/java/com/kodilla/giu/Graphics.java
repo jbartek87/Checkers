27584f610dd5ad17a5d066f7cac6376609cc3364
jbartek87 <jbartek87@github.com>
2019-12-02 20:30:59 +0100

    Checkers

--- src/main/java/com/kodilla/giu/Graphics.java
+++ src/main/java/com/kodilla/giu/Graphics.java
@@ -1,6 +1,7 @@
 package com.kodilla.giu;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,7 +13,9 @@ import javafx.stage.Stage;
 public class Graphics extends Application {
     private Image imageback = new Image("file:C:\\Users\\cp24\\Desktop\\JavaSptember\\Kodilla\\checkersV3\\src\\main\\resources\\board.png");
     Image image = new Image("file:C:\\Users\\cp24\\Desktop\\JavaSptember\\Kodilla\\checkersV3\\src\\main\\resources\\pawn.png");
-    ImageView iv1 = new ImageView();
+    private FlowPane images = new FlowPane(Orientation.HORIZONTAL);
+
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -22,16 +25,19 @@ public class Graphics extends Application {
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
-        iv1.setImage(image);
-        iv1.setPreserveRatio(false);
+
 
         GridPane grid = new GridPane();
-
         grid.setBackground(background);
 
+        ImageView img = new ImageView(image);
 
-        Scene scene = new Scene(grid, 1600, 900, Color.CORAL);
-        grid.getChildren().add(iv1);
+        images.getChildren().add(img);
+        img.setX(200);
+        grid.add(images, 10,0,3,1);
+        Scene scene = new Scene(grid, 1600, 900, Color.CADETBLUE);
+
+
         primaryStage.setTitle("Checkers");
         primaryStage.setScene(scene);
         primaryStage.show();
