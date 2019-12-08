fda39b498bf398119eb795486fcd638148464c58
jbartek87 <jbartek87@github.com>
2019-12-08 19:31:29 +0100

    checkers

--- src/main/java/com/kodilla/logic/Board.java
+++ src/main/java/com/kodilla/logic/Board.java
@@ -3,8 +3,12 @@ package com.kodilla.logic;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.kodilla.logic.FigureColor.BLACK;
+import static com.kodilla.logic.FigureColor.WHITE;
+
 public class Board {
     private List<BoardRow> rows = new ArrayList<>();
+    private FigureColor whichMove = WHITE;
 
     public Board() {
         for (int n = 0; n < 10; n++) {
@@ -12,45 +16,46 @@ public class Board {
         }
     }
 
-    public Figure getFigure(int col, int row){
+    public Figure getFigure(int col, int row) {
         return rows.get(row).getCols().get(col);
     }
 
-    public void setFigure(int col, int row, Figure figure){
+    public void setFigure(int col, int row, Figure figure) {
         rows.get(row).getCols().add(col, figure);
         rows.get(row).getCols().remove(col + 1);
     }
 
+
     public void initBoard() {
 
-        setFigure(0, 0, new Pawn(FigureColor.WHITE));
-        setFigure(2, 0, new Pawn(FigureColor.WHITE));
-        setFigure(4, 0, new Pawn(FigureColor.WHITE));
-        setFigure(6, 0, new Pawn(FigureColor.WHITE));
-        setFigure(1, 1, new Pawn(FigureColor.WHITE));
-        setFigure(3, 1, new Pawn(FigureColor.WHITE));
-        setFigure(5, 1, new Pawn(FigureColor.WHITE));
-        setFigure(7, 1, new Pawn(FigureColor.WHITE));
-        setFigure(0, 2, new Pawn(FigureColor.WHITE));
-        setFigure(2, 2, new Pawn(FigureColor.WHITE));
-        setFigure(4, 2, new Pawn(FigureColor.WHITE));
-        setFigure(6, 2, new Pawn(FigureColor.WHITE));
+        setFigure(0, 0, new Pawn(WHITE));
+        setFigure(2, 0, new Pawn(WHITE));
+        setFigure(4, 0, new Pawn(WHITE));
+        setFigure(6, 0, new Pawn(WHITE));
+        setFigure(1, 1, new Pawn(WHITE));
+        setFigure(3, 1, new Pawn(WHITE));
+        setFigure(5, 1, new Pawn(WHITE));
+        setFigure(7, 1, new Pawn(WHITE));
+        setFigure(0, 2, new Pawn(WHITE));
+        setFigure(2, 2, new Pawn(WHITE));
+        setFigure(4, 2, new Pawn(WHITE));
+        setFigure(6, 2, new Pawn(WHITE));
 
 
-        setFigure(0, 6, new Pawn(FigureColor.BLACK));
-        setFigure(1, 7, new Pawn(FigureColor.BLACK));
-        setFigure(1, 5, new Pawn(FigureColor.BLACK));
-        setFigure(2, 6, new Pawn(FigureColor.BLACK));
-        setFigure(3, 7, new Pawn(FigureColor.BLACK));
-        setFigure(3, 5, new Pawn(FigureColor.BLACK));
-        setFigure(4, 6, new Pawn(FigureColor.BLACK));
-        setFigure(5, 7, new Pawn(FigureColor.BLACK));
-        setFigure(5, 5, new Pawn(FigureColor.BLACK));
-        setFigure(6, 6, new Pawn(FigureColor.BLACK));
-        setFigure(7, 7, new Pawn(FigureColor.BLACK));
-        setFigure(7, 5, new Pawn(FigureColor.BLACK));
+        setFigure(0, 6, new Pawn(BLACK));
+        setFigure(1, 7, new Pawn(BLACK));
+        setFigure(1, 5, new Pawn(BLACK));
+        setFigure(2, 6, new Pawn(BLACK));
+        setFigure(3, 7, new Pawn(BLACK));
+        setFigure(3, 5, new Pawn(BLACK));
+        setFigure(4, 6, new Pawn(BLACK));
+        setFigure(5, 7, new Pawn(BLACK));
+        setFigure(5, 5, new Pawn(BLACK));
+        setFigure(6, 6, new Pawn(BLACK));
+        setFigure(7, 7, new Pawn(BLACK));
+        setFigure(7, 5, new Pawn(BLACK));
 
-
+        System.out.println("Color" + " " + whichMove + " " + "starts the game");
     }
 
     private void doSimpleMove(int col1, int row1, int col2, int row2) {
@@ -63,32 +68,32 @@ public class Board {
         Figure figure = getFigure(col1, row1);
         Figure moveFigure = getFigure(col2, row2);
         setFigure(col1, row1, new None());
-        if (figure.getColor() == FigureColor.WHITE && col2 > col1 && row2 > row1) {
+        if (figure.getColor() == WHITE && col2 > col1 && row2 > row1) {
             setFigure(col2 + 1, row2 + 1, figure);
             setFigure(col2, row2, new None());
-        } else if (figure.getColor() == FigureColor.WHITE && col1 > col2 && row2 > row1) {
+        } else if (figure.getColor() == WHITE && col1 > col2 && row2 > row1) {
             setFigure(col2 - 1, row2 + 1, figure);
             setFigure(col2, row2, new None());
 
-        } else if (figure.getColor() == FigureColor.WHITE && col1 > col2 && row1 > row2) {
+        } else if (figure.getColor() == WHITE && col1 > col2 && row1 > row2) {
             setFigure(col2 - 1, row2 - 1, figure);
             setFigure(col2, row2, new None());
 
-        } else if (figure.getColor() == FigureColor.WHITE && col2 > col1 && row1 > row2) {
+        } else if (figure.getColor() == WHITE && col2 > col1 && row1 > row2) {
             setFigure(col2 + 1, row2 - 1, figure);
             setFigure(col2, row2, new None());
 
-        } else if (figure.getColor() == FigureColor.BLACK && col2 < col1 && row1 > row2) {
+        } else if (figure.getColor() == BLACK && col2 < col1 && row1 > row2) {
             setFigure(col2 - 1, row2 - 1, figure);
             setFigure(col2, row2, new None());
-        } else if (figure.getColor() == FigureColor.BLACK && col2 > col1 && row1 > row2) {
+        } else if (figure.getColor() == BLACK && col2 > col1 && row1 > row2) {
             setFigure(col2 + 1, row2 - 1, figure);
             setFigure(col2, row2, new None());
-        } else if (figure.getColor() == FigureColor.BLACK && col2 > col1 && row2 > row1) {
+        } else if (figure.getColor() == BLACK && col2 > col1 && row2 > row1) {
             setFigure(col2 + 1, row2 + 1, figure);
             setFigure(col2, row2, new None());
 
-        } else if (figure.getColor() == FigureColor.BLACK && col1 > col2 && row2 > row1) {
+        } else if (figure.getColor() == BLACK && col1 > col2 && row2 > row1) {
             setFigure(col2 - 1, row2 + 1, figure);
             setFigure(col2, row2, new None());
 
@@ -99,10 +104,10 @@ public class Board {
     private boolean isSimpleMoveValid(int col1, int row1, int col2, int row2) {
         Figure figure = getFigure(col1, row1);
         Figure moveFigure = getFigure(col2, row2);
-        if (figure.getColor() == FigureColor.WHITE && col2 - col1 == 1 && row2 - row1 == 1 && moveFigure instanceof None
-                || figure.getColor() == FigureColor.WHITE && col1 - col2 == 1 && row2 - row1 == 1 && moveFigure instanceof None
-                || figure.getColor() == FigureColor.BLACK && col2 - col1 == 1 && row1 - row2 == 1 && moveFigure instanceof None
-                || figure.getColor() == FigureColor.BLACK && col1 - col2 == 1 && row1 - row2 == 1 && moveFigure instanceof None) {
+        if (figure.getColor() == WHITE && col2 - col1 == 1 && row2 - row1 == 1 && moveFigure instanceof None
+                || figure.getColor() == WHITE && col1 - col2 == 1 && row2 - row1 == 1 && moveFigure instanceof None
+                || figure.getColor() == BLACK && col2 - col1 == 1 && row1 - row2 == 1 && moveFigure instanceof None
+                || figure.getColor() == BLACK && col1 - col2 == 1 && row1 - row2 == 1 && moveFigure instanceof None) {
             System.out.println("Simple move is true");
             return true;
         }
@@ -117,14 +122,14 @@ public class Board {
             System.out.println("Hit move is false");
             return false;
         } else if (
-                figure.getColor() == FigureColor.WHITE && col2 - col1 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 + 1) instanceof None
-                        || figure.getColor() == FigureColor.WHITE && col1 - col2 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 + 1) instanceof None
-                        || figure.getColor() == FigureColor.WHITE && col1 - col2 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 - 1) instanceof None
-                        || figure.getColor() == FigureColor.WHITE && col2 - col1 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 - 1) instanceof None
-                        || figure.getColor() == FigureColor.BLACK && col1 - col2 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 - 1) instanceof None
-                        || figure.getColor() == FigureColor.BLACK && col2 - col1 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 - 1) instanceof None
-                        || figure.getColor() == FigureColor.BLACK && col2 - col1 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 + 1) instanceof None
-                        || figure.getColor() == FigureColor.BLACK && col1 - col2 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 + 1) instanceof None) {
+                figure.getColor() == WHITE && col2 - col1 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 + 1) instanceof None
+                        || figure.getColor() == WHITE && col1 - col2 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 + 1) instanceof None
+                        || figure.getColor() == WHITE && col1 - col2 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 - 1) instanceof None
+                        || figure.getColor() == WHITE && col2 - col1 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 - 1) instanceof None
+                        || figure.getColor() == BLACK && col1 - col2 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 - 1) instanceof None
+                        || figure.getColor() == BLACK && col2 - col1 == 1 && row1 - row2 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 - 1) instanceof None
+                        || figure.getColor() == BLACK && col2 - col1 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 + 1, row2 + 1) instanceof None
+                        || figure.getColor() == BLACK && col1 - col2 == 1 && row2 - row1 == 1 && moveFigure instanceof Pawn && getFigure(col2 - 1, row2 + 1) instanceof None) {
 
             System.out.println("Hit move is true");
             return true;
@@ -133,18 +138,29 @@ public class Board {
         return false;
     }
 
+    public void setOppositeColor() {
+        if (whichMove == WHITE) {
+            whichMove = BLACK;
+        } else if (whichMove == BLACK) {
+            whichMove = WHITE;
+        }
 
-    public void move(int col1, int row1, int col2, int row2) throws IndexOutOfBoundsException {
-        try {
+    }
+
+    public void move(int col1, int row1, int col2, int row2) {
+
+        if (getFigure(col1, row1).getColor() == whichMove) {
             if (isSimpleMoveValid(col1, row1, col2, row2)) {
                 doSimpleMove(col1, row1, col2, row2);
+                setOppositeColor();
             } else if (isHitMoveValid(col1, row1, col2, row2)) {
                 doHitMove(col1, row1, col2, row2);
+
             }
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Invalid move");
+            System.out.println("Now it's color" + " " + whichMove + " " + "turn");
         }
     }
+
 
 
     @Override
@@ -157,7 +173,7 @@ public class Board {
                 if(figure instanceof None){
                     s+="  ";
                 } else {
-                    s+=(figure.getColor() == FigureColor.WHITE) ? "w" : "b";
+                    s += (figure.getColor() == WHITE) ? "w" : "b";
                     s+=(figure instanceof Queen) ? "Q" : "P";
                 }
                 s+="|";
